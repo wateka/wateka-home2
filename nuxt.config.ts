9cb1b2e514b7e39ecec1e6f8ec6f3da479f60ba1
wateka <66128234+wateka@users.noreply.github.com>
2023-10-22 04:02:34 +0900

    headのtitle, htmlAttrsを追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,10 @@ export default defineNuxtConfig({
   ],
   app: {
     head: {
+      htmlAttrs: {
+        lang: "ja"
+      },
+      title: "wateka.dev",
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
       link: [
